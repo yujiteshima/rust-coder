23e1b75d2a96cbc8de404958ec5ed7b31132d3e0
yujiteshima <yuji.teshima@gmail.com>
2023-10-09 05:58:39 +0900

    "ac"

--- tessoku-book/src/bin/a02.rs
+++ tessoku-book/src/bin/a02.rs
@@ -1,3 +1,29 @@
+use std::io;
+
 fn main() {
-    todo!();
+    // input
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).ok();
+    let vec: Vec<&str> = input.split_whitespace().collect();
+    let n_num = vec[0].trim().parse().unwrap();
+    let x = vec[1].trim().parse().unwrap_or(0);
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).ok();
+    let vec: Vec<&str> = input.split_whitespace().collect();
+    let mut vec1 = vec![];
+    for i in 0..n_num{
+        vec1.push(vec[i].trim().parse().unwrap_or(0));
+    }
+
+    // 処理
+    let mut flag= "No";
+    for _i in 0..n_num{
+        if vec1[_i] == x {
+            flag = "Yes";
+        }
+    }
+
+    // output
+    println!("{}", flag);
 }
