1654a6c6eaa40ba42c8aef2f3b6e3870094334e1
yujiteshima <yuji.teshima@gmail.com>
2023-10-17 03:36:14 +0900

    ac

--- abc324/src/bin/b.rs
+++ abc324/src/bin/b.rs
@@ -1,3 +1,14 @@
+use proconio::input;
 fn main() {
-    todo!();
+    input! {
+        mut n:usize
+    }
+    while n % 2 == 0 {
+        n /= 2;
+    }
+    while n % 3 == 0 {
+        n /= 3;
+    }
+    if n == 1 { println!("Yes"); }
+    else { println!("No");}
 }
