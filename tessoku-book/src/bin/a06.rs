ddf292a038bda9c50cd9aacb377f773e338554b3
yujiteshima <yuji.teshima@gmail.com>
2023-10-17 01:32:11 +0900

    ac

--- tessoku-book/src/bin/a06.rs
+++ tessoku-book/src/bin/a06.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        Q: usize,
+        A: [usize; N],
+        list: [[usize; 2]; Q],
+    }
+
+    let mut sum_list = vec![];
+    sum_list.push(0);
+    for i in A {
+        sum_list.push(sum_list.last().unwrap() + i);
+    }
+    for i in 0..Q {
+        println!("{}", sum_list[list[i][1]] - sum_list[list[i][0] - 1])
+    }
 }
