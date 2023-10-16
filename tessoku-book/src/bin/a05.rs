c544203c3b468b079b83a887bb60ebecbe4e8bab
yujiteshima <yuji.teshima@gmail.com>
2023-10-16 23:17:11 +0900

    ac

--- tessoku-book/src/bin/a05.rs
+++ tessoku-book/src/bin/a05.rs
@@ -1,3 +1,16 @@
+use proconio::input;
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        k: i32
+    }
+    let mut ans = 0;
+    for i in 1..=n {
+        for j in 1..=n {
+               if k - i - j <= n && k - i - j >= 1{
+                   ans += 1;
+               }
+           }
+    }
+    println!("{}", ans);
 }
