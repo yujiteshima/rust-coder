1a08d63630a5e47246b5a8c96c49939983cdc984
yujiteshima <yuji.teshima@gmail.com>
2023-10-16 22:51:32 +0900

    ac

--- tessoku-book/src/bin/a04.rs
+++ tessoku-book/src/bin/a04.rs
@@ -1,3 +1,17 @@
+use proconio::input;
 fn main() {
-    todo!();
+    input!{
+        n: i64
+    }
+    print!("{}", shinsu(n, 2));
+}
+
+fn shinsu(mut x:i64, b: i64) -> String {
+    let mut ans = String::new();
+    for _ in (0..10).rev() {
+        let value = x % 2;
+        ans.push_str(&value.to_string());
+        x /= b;
+    }
+    ans.chars().rev().collect()
 }
